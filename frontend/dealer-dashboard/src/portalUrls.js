13b52c30f578c41bde7142d272ef687ef94790e2
shravaniii2606 <shravaniii2619@gmail.com>
2026-06-21 13:57:58 +0530

    Update portalUrls.js

--- frontend/dealer-dashboard/src/portalUrls.js
+++ frontend/dealer-dashboard/src/portalUrls.js
@@ -1,12 +1,6 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 export const PORTAL_URLS = {
-  gov:
-    isDevelopment
-      ? 'http://localhost:5173'
-      : 'https://gov.KisanSetu.com',
-  dealer:
-    isDevelopment
-      ? 'http://localhost:5174'
-      : 'https://dealer.KisanSetu.com',
+  gov: isDevelopment ? 'http://localhost:5173' : 'https://kisan-setu-1eok.vercel.app',
+  dealer: isDevelopment ? 'http://localhost:5174' : 'https://kisan-setu-ivij.vercel.app',
 };
